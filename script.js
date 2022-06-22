20d24732dd31d3d0d4ef0c614776cfddab98b5b4
Jimoh Sodiq <serikidon32@gmail.com>
2022-06-22 14:37:59 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = "Jimoh Sodiq";
+let height = 5.7;
+let country = "Nigeria";
+
+console.log(`My name is ${name}`);
+console.log(`I am ${height} tall in height`);
+console.log(`The name of my country is ${country}`);
